89ec9ee5fa216e11eb49c3a7460b0b62856b12d0
haha07 <wldnjs20110@gmail.com>
2015-04-23 01:22:39 +0900

    로그인창 처리 동적

--- public/mainjs/index.js
+++ public/mainjs/index.js
@@ -3,6 +3,8 @@
 function fn_login(){
 	
 	 $.get('/login', function (data) {
+		 var div = "<div class='contact'></div>"
+		$('.container').append(div);
          var html = data;
          $('.contact').html(html);
 	 });
